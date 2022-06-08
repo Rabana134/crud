bb5906fb6a21dcf989dcfd238c48f06566e2187a
maola rabana <maolarabana@gmail.com>
2022-06-07 18:42:29 -1200

    Updates

---
+++ precache-manifest.92b006c0278671c1e307d2b79c703b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a7ced1270e10f6e0aa4910de348adc",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "5315fdcb0eb3e3e62f2d",
+    "url": "/crud/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1619d9b5d1f7db44b0d9",
+    "url": "/crud/static/css/main.3224a1ca.chunk.css"
+  },
+  {
+    "revision": "5315fdcb0eb3e3e62f2d",
+    "url": "/crud/static/js/2.cc8f7b60.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crud/static/js/2.cc8f7b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1619d9b5d1f7db44b0d9",
+    "url": "/crud/static/js/main.acb07d49.chunk.js"
+  },
+  {
+    "revision": "21700fa7b7ed9b3c5261",
+    "url": "/crud/static/js/runtime-main.3ead6e79.js"
+  }
+]);
